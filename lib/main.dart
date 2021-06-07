ef482dc71621158c029ab6cec1539b10ff194798
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-06-07 14:56:06 +0900

    Flutter UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/Navbar/Navbar.dart';
-import 'package:myapp/LandingPage/LandingPage.dart';
+import 'package:myapp/Navbar.dart';
+import 'package:myapp/DashBoard.dart';
+import 'package:myapp/Profile.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,26 +25,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-            Color.fromRGBO(195, 20, 50, 1.0),
-            Color.fromRGBO(36, 11, 54, 1.0)
-          ])),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Navbar(),
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                child: LandingPage())
-          ],
-        ),
-      ),
+        body: Row(
+      children: [
+        Navbar(),
+        DashBoard(),
+        Profile(),
+      ],
     ));
   }
 }
